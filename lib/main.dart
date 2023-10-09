41dc59f1d1eb9d81df59337b480164faf2665690
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-10-10 01:10:16 +0530

    intialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ubixstar_intern_assignment/firebase_options.dart';
 import 'package:ubixstar_intern_assignment/provider.dart';
 import 'package:ubixstar_intern_assignment/screens/first_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
