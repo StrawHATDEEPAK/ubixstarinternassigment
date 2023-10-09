c102b57271aad7e05501952c2a6d64f9b57bdf73
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-10-09 22:43:07 +0530

    Refactored textfield

---
+++ lib/widgets/textfield_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Enter Text",
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
